05a7cdda0c35ec91b003cb164d7d950b195db218
MahmutEngin <155198520+MahmutEngin@users.noreply.github.com>
2025-04-27 00:10:49 +0200

    Update Mahmut_Engin_TL_Week_2.py

--- Mahmut_Engin_TL_Week_2.py
+++ Mahmut_Engin_TL_Week_2.py
@@ -1,29 +1,3 @@
-"""Soru1: Öğrenci Notlarının İşlenmesi
-Bir öğrencinin notlarını işlemek için bir Python programı yazmanız gerekir. Programın aşağıdaki işlevleri gerçekleştirmesi gerekir:
-
-Bir sözlük kullanarak 10 öğrenci için bilgi ve notları depolayın. Her öğrencinin adını, soyadını ve notlarını (Vize, Final ve Sözlü notları) olsun. Örneğin:
-öğrenciler = {
-'Ahmet Yılmaz': [85, 90, 78], # Vize: 85, Final: 90, Sözlü: 78
-'Mehmet Demir': [82, 86, 76], # Vize: 82, Final: 86, Sözlü: 76
-'Ayşe Kaya': [78, 89, 95], # Vize: 78, Final: 89, Sözlü: 95
-'Zeynep Çelik': [65, 70, 80], # Vize: 65, Final: 70, Sözlü: 80 
-'Ali Kara': [50, 60, 75], # Arasınav: 50, Final: 60, Sözlü: 75 
-'Fatma Yıldız': [88, 95, 80], # Arasınav: 88, Final: 95, Sözlü: 80 
-'Murat Aydın': [67, 68, 74], # Arasınav: 67, Final: 68, Sözlü: 74 
-'Elif Aksoy': [95, 90, 85], # Arasınav: 95, Final: 90, Sözlü: 85 
-'Hakan Öztürk': [45, 70, 54], # Arasınav: 45, Final: 70, Sözlü: 54 
-'Canan Taş': [80, 75, 82] # Vize: 80, Final: 75, Sözlü: 82
-}
-
-1-Her öğrencinin not ortalamasını hesapla ve sözlüğe ekle.
-
-2-En yüksek not ortalamasına sahip öğrenciyi bul ve ekrana yazdır.
-
-3-Her öğrencinin adını soyadından ayır ve ayrı bir tuple'da sakla ve bir listeye ekle.
-
-4-Adları alfabetik sıraya göre sırala ve sıralı listeyi ekrana yazdır.
-
-5-Not ortalaması 70'in altında olan öğrencileri bir kümede (kümede) tut."""
 ogrenciler = {
     'Ahmet Yılmaz': [85, 90, 78], 
     'Mehmet Demir': [82, 86, 76], 
