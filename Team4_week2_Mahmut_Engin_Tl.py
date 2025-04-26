b1bdd4d3a82f6b4cc254749a0ac59a8e90d1c1d5
Mahmut Engin <155198520+MahmutEngin@users.noreply.github.com>
2025-04-27 00:36:51 +0200

    Team4_week2_Mahmut_Engin_Tl.py

--- Team4_week2_Mahmut_Engin_Tl.py
+++ Team4_week2_Mahmut_Engin_Tl.py
@@ -1,48 +1,93 @@
-"""Question1: Student Grades Processing
-You need to write a Python program to process a student's grades. The program needs to perform the following functions:
-
-Store information and notes for 10 students using a dictionary. Let each student have their name, surname and grades (Midterm, Final and Oral grades). For example:
-students = {
-    'Ahmet Yılmaz': [85, 90, 78],  # Midterm: 85, Final: 90, Oral: 78
-    'Mehmet Demir': [82, 86, 76],  # Midterm: 82, Final: 86, Oral: 76
-    'Ayşe Kaya': [78, 89, 95],     # Midterm: 78, Final: 89, Oral: 95
-    'Zeynep Çelik': [65, 70, 80],  # Midterm: 65, Final: 70, Oral: 80
-    'Ali Kara': [50, 60, 75],      # Midterm: 50, Final: 60, Oral: 75
-    'Fatma Yıldız': [88, 95, 80],  # Midterm: 88, Final: 95, Oral: 80
-    'Murat Aydın': [67, 68, 74],   # Midterm: 67, Final: 68, Oral: 74
-    'Elif Aksoy': [95, 90, 85],     # Midterm: 95, Final: 90, Oral: 85
-    'Hakan Öztürk': [45, 70, 54],   # Midterm: 45, Final: 70, Oral: 54
-    'Canan Taş': [80, 75, 82]      # Midterm: 80, Final: 75, Oral: 82
+# Python_Modul_Week_2
+### Question1: Student Grades Processing
+ogrenciler = {
+    'Ahmet Yılmaz': [85, 90, 78], 
+    'Mehmet Demir': [82, 86, 76], 
+    'Ayşe Kaya': [78, 89, 95], 
+    'Zeynep Çelik': [65, 70, 80], 
+    'Ali Kara': [50, 60, 75], 
+    'Fatma Yıldız': [88, 95, 80], 
+    'Murat Aydın': [67, 68, 74], 
+    'Elif Aksoy': [95, 90, 85], 
+    'Hakan Öztürk': [45, 70, 54], 
+    'Canan Taş': [80, 75, 82]
 }
+# 1-Her öğrencinin not ortalamasını hesapla ve sözlüğe ek
+ortalamalar={}
+for ogrenci in ogrenciler:
+    notlar=ogrenciler[ogrenci]
+    ort =sum(notlar) / len(notlar)
+    ogrenciler[ogrenci].append(ort)
+    ortalamalar[ogrenci] = ort
+print(ogrenciler)
+# 2-En yüksek not ortalamasına sahip öğrenciyi bul ve ekrana yazdır.
+maximum =max(ortalamalar.values())
+en_yuksek_ogrenci = max(ortalamalar, key=ortalamalar.get)
+print(f' En yuksek ortalaya sahip ogrenci {en_yuksek_ogrenci} Notu: {maximum}')
+# 3-Her öğrencinin adını soyadından ayır ve ayrı bir tuple'da sakla ve bir listeye ekle.
+isim ={}
+soyisim={}
+for ogrenci in ogrenciler:
+    ad, soyad =ogrenci.split()
+    isim[ogrenci]= ad
+    soyisim[ogrenci]=soyad
+    adlar=tuple(isim.values())
+    soyadlar=tuple(soyisim.values())
+
+print(adlar)
+print(soyadlar)
+# 4-Adları alfabetik sıraya göre sırala ve sıralı listeyi ekrana yazdır.
+alfabetik=sorted(ogrenciler.keys())
+sirali_liste={sirali: ogrenciler[sirali] for sirali in alfabetik}
+print(sirali_liste)
+# 5-Not ortalaması 70'in altında olan öğrencileri bir kümede (kümede) tut.
+dusuk =set()
+for ogrenci in ogrenciler:
+    notlar=ogrenciler[ogrenci]
+    ort =sum(notlar) / len(notlar)
+    if ort <70:
+        dusuk.add(ogrenci)
+print(dusuk)
 
 
-1-Calculate each student's GPA and add it to the dictionary.
 
-2-Find the student with the highest GPA and print it on the screen.
-
-3- Separate each student's name from their surname and store them in a separate tuple and add them to a list.
-
-4-Sort the names in alphabetical order and print the sorted list on the screen.
-
-5-Keep students with a GPA below 70 in a cluster (set)."""
-
-
-
-
-
-
-
-# Question 2: Film Library Management System Project
-# Project Description: This project aims to create an application that will help the user manage their movie collection. Users can add, edit, delete movies and view their collection.
-
-# Data Structures Used: Dictionaries (to store movies and related information), lists (to display movie collection)
-
-# Basic Functions:
-
-# 1-Create a movie data by taking information such as movie name, director, release year and genre from the user and store it in a dictionary.
-
-# 2-Give the user the option to edit or delete a movie. (For this, a suitable function must be written for whatever data they want to change about the movie.)
-
-# 3-Allow the user to view their collection. List all movies or filter by criteria such as genre or year of release.
-
-# 4-Save the movie data in a file and restore this data when you start the program.
+### Question 3: Customer Management System
+def menu():
+    print("""
+        1) Yemi musteri eklemek icin "1" e basiniz.
+        2) Musteri bilgilerini duzenlemek icin "2" e basiniz.
+        3) Musteri bil;gilerini silmek icin "3" e basiniz.
+        4) Musterileri listelemek icin "4" e basiniz.
+        4) Cikmak icin "q" e basiniz.
+        """) 
+musteriler={}
+while True:
+    menu()
+    secim = input("Seciminizi yapiniz: ")
+    if secim in  ["1","2","3","4","q"]:
+        if secim=="q":
+           break
+        if secim=="1": # musteri Ekleme
+           ide = input("Musteri idesini giriniz:")          
+           ad= input("Musteri adini giriniz:")
+           soyad= input("Musteri soyadini giriniz:")
+           e_posta = input("Musteri mail adresini giriniz:")
+           telefon = input("Musteri telefon numarasini giriniz:")
+           musteriler[ide]=[ad,soyad,e_posta,telefon]
+           print(musteriler)
+        if secim=="2":
+           ide = input("Musteri idesini giriniz:")    
+           if ide in musteriler:
+                ad = input("Yeni ad (degisiklik olmayacaksa enter'a basiniz) ")
+                soyad = input("Yeni soyad (degisiklik olmayacaksa enter'a basiniz) : ")
+                e_posta = input("Yeni e-posta (degisiklik olmayacaksa enter'a basiniz)  : ")
+                telefon = input("Yeni telefon (degisiklik olmayacaksa enter'a basiniz) : ")
+                if ad:
+                    musteriler[ide][0]=ad
+                if soyad:
+                    musteriler[ide][1]=soyad
+                if e_posta:
+                    musteriler[ide][2]=e_posta
+                if  telefon:
+                    musteriler[ide][3]= telefon
+                print(musteriler)
